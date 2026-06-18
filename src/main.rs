e3af8d505b03dbe8cd4f48513b46f26b565204a9
stefand-0 <stefandragan95@gmail.com>
2026-06-18 19:54:13 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,41 +5,19 @@ use pest::Parser;
 
 mod ast;
 mod emitter;
-mod parser; // parser struct 
+mod parser;
+mod transformer;
 
 use crate::parser::{NeoParser, Rule};
 use crate::emitter::Emitter;
 
 fn main() {
-    // Get command line arguments
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: neo_compiler <input_file.neoh>");
-        process::exit(1);
-    }
-
-    let input_path = &args[1];
-
-    // Read the source code
-    let source_code = fs::read_to_string(input_path).expect("Could not read file");
-
-    // 3Parse the source code
-    let pairs = NeoParser::parse(Rule::file, &source_code).unwrap_or_else(|e| {
-        eprintln!("Parsing Error: {}", e);
-        process::exit(1);
-    });
-
-    // Build AST `src/transformer.rs`)
-    // This is the bridge between the raw pest 'Pairs' and your 'ast.rs' structs
-    let ast_data = transformer::build_ast(pairs);
-
-    // Emit SystemVerilog
+    if args.len() < 2 { process::exit(1); }
+    let source = fs::read_to_string(&args[1]).unwrap();
+    let pairs = NeoParser::parse(Rule::file, &source).unwrap();
+    let ast = transformer::build_ast(pairs);
     let mut emitter = Emitter::new();
-    emitter.emit_file(&ast_data);
-
-    // Write output
-    let output_path = input_path.replace(".neoh", ".sv");
-    fs::write(&output_path, emitter.output).expect("Could not write SV file");
-
-    println!("Successfully compiled {} -> {}", input_path, output_path);
+    emitter.emit_file(&ast);
+    fs::write(args[1].replace(".neoh", ".sv"), emitter.output).unwrap();
 }
