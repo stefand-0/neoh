e9913683cd46f04f0270619f2dd5c97199fa66cc
stefand-0 <stefandragan95@gmail.com>
2026-06-18 19:53:52 +0300

    Update transformer.rs

--- src/transformer.rs
+++ src/transformer.rs
@@ -18,19 +18,3 @@ fn build_block(pair: pest::iterators::Pair<Rule>) -> BlockDef {
     let name = inner.next().unwrap().as_str().to_string();
     BlockDef { name, params: vec![], body: vec![] }
 }
-
-pub fn build_put(pair: pest::iterators::Pair<Rule>) -> PutStmt {
-    let mut inner = pair.into_inner();
-    let target = inner.next().unwrap().as_str().to_string();
-    let op = inner.next().unwrap().as_str().to_string();
-    let expr = inner.next().unwrap().as_str().to_string();
-    let width = inner.find_map(|p| if p.as_rule() == Rule::width_constraint { Some(parse_width(p)) } else { None });
-    PutStmt { target, op, expr, width }
-}
-
-fn parse_width(pair: pest::iterators::Pair<Rule>) -> WidthConstraint {
-    let mut inner = pair.into_inner();
-    let msb = inner.next().unwrap().as_str().parse().unwrap();
-    let lsb = inner.next().unwrap().as_str().parse().unwrap();
-    WidthConstraint { msb, lsb }
-}
