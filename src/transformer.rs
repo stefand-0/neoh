b76ef56229e5e388a1edd6d2af7a744de90fa4bc
stefand-0 <stefandragan95@gmail.com>
2026-06-19 13:04:21 +0300

    Update transformer.rs

--- src/transformer.rs
+++ src/transformer.rs
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2026 stefand-0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 use crate::ast::*;
 use crate::parser::Rule;
 use pest::iterators::{Pair, Pairs};
@@ -143,3 +160,7 @@ fn parse_width(pair: Pair<Rule>) -> WidthConstraint {
     let mut inner = pair.into_inner();
     WidthConstraint { msb: inner.next().unwrap().as_str().parse().unwrap(), lsb: inner.next().unwrap().as_str().parse().unwrap() }
 }
+
+/*
+This codename was really wierd... I had to learn about "pest".
+ */
