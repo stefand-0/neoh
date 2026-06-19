c167bbe8b98176eb76d83e478dbcb45bb6d06683
stefand-0 <stefandragan95@gmail.com>
2026-06-19 20:24:20 +0300

    .

--- test.sv
+++ test.sv
@@ -1,4 +1,15 @@
-`timescale 1ns / 1ps
+module Adder(
+);
 
-module Example();
+  logic res;
+  assign res = 1;
+  assign c = a + b;
 endmodule
+
+module TestAdder_tb;
+  Adder uut();
+  initial begin
+    #10 $display("Simulation Started");
+  end
+endmodule
+
